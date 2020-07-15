94c35776e831681992c088a8eaab8de62e9b936a
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-07-15 18:00:49 -0300

    spin_words() method and reverse_word() method created.

--- spin_words.py
+++ spin_words.py
@@ -3,37 +3,36 @@
 #               and returns the same string, but with all five or more letter
 #               words reversed
 # Initial date: July 13, 2020
-# Last update:  July 13, 2020
+# Last update:  July 15, 2020
 
 class SpinWords:
-    def __init__(self, sentence):
-        self.sentence = sentence
+    def spin_words(self, sentence):
+        words = sentence.split()
+        newSentence = ''
 
-    def spinWords(self):
-        sentence = self.sentence.split(" ")
-        spunWords = ""
-
-        for word in sentence:
+        for word in words:
             if len(word) >= 5:
-                currentWord = []
-                lengthWord = 0
-                while lengthWord < len(word):
-                    currentWord.append(word[lengthWord])
-                    lengthWord = lengthWord + 1
-                currentWord.reverse()
-
-                for newLetter in currentWord:
-                    spunWords = spunWords + newLetter
-                spunWords = spunWords + " "
+                spin_word = self.reverse_word(word)
+                newSentence = newSentence + ' ' + spin_word
             else:
-                spunWords = spunWords + word + " "
-        return  spunWords
+                newSentence = newSentence + ' ' + word
+
+        newSentence = newSentence.strip()
+        return newSentence
+
+    def reverse_word(self, word):
+        spin_word = ''
+
+        for caracter in word:
+            spin_word = caracter + spin_word
+
+        return spin_word
 
 if __name__ == '__main__':
-    words = input("Please, enter one or more words: ")
-    print("Typed words: " + words)
+    sentence = input("Please, enter one or more words: ")
+    print("Typed words: " + sentence)
 
-    spin = SpinWords(words)
-    spunWords = spin.spinWords()
+    spin = SpinWords()
+    new_sentence = spin.spin_words(sentence)
 
-    print("Spun words: " + spunWords)
+    print("{} is equal to {} ." .format(sentence, new_sentence))
