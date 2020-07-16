1ba0c30cdc381b07683fbf71d57d125d7579a2a7
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-07-16 16:54:15 -0300

    Exception for division by 0 was added.

--- bit_counting.py
+++ bit_counting.py
@@ -7,24 +7,28 @@
 
 class BitCounting:
     def count_bits_equal_one(self, number):
-        binary_representation = []
-        bits_equal_one = 0
+        if number == 0:
+            print("It is not possible to make this operation with number 0.")
+            return None
+        else:
+            binary_representation = []
+            bits_equal_one = 0
 
-        while int(number != 1):
-            binary_representation.append(int(number % 2))
-            if int(number % 2) == 1:
+            while int(number != 1):
+                binary_representation.append(int(number % 2))
+                if int(number % 2) == 1:
+                    bits_equal_one = bits_equal_one + 1
+
+                number = int(number / 2)
+            if number == 1:
+                binary_representation.append(1)
                 bits_equal_one = bits_equal_one + 1
 
-            number = int(number / 2)
-        if number == 1:
-            binary_representation.append(1)
-            bits_equal_one = bits_equal_one + 1
+            binary_representation_number = self.display_binary_representation(binary_representation)
+            print("The number that you entered has {} bits equal 1.".format(bits_equal_one))
+            print("Binary representation: {}" .format(binary_representation_number))
 
-        binary_representation_number = self.display_binary_representation(binary_representation)
-        print("The number that you entered has {} bits equal 1.".format(bits_equal_one))
-        print("Binary representation: {}" .format(binary_representation_number))
-
-        return bits_equal_one
+            return bits_equal_one
 
     def display_binary_representation(self, binary_representation = []):
         binary_representation_number = ''
